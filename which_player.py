d0e94f24a68c89f7ffd12bf1de6010a03d84fa7f
Reece White <reecethomaswhite@gmail.com>
2022-08-28 20:45:19 +1000

    Get players in price and position

--- which_player.py
+++ which_player.py
@@ -1,17 +1,51 @@
 import asyncio
 import aiohttp
+import sys
+import argparse
 from fpl import FPL
+
 
 # Need to set system environment variables FPL_EMAIL and FPL
 async def main():
     async with aiohttp.ClientSession() as session:
         fpl = FPL(session)
         await fpl.login()
-        user = await fpl.get_user()
-        my_team = await user.get_team()
+        players = await fpl.get_players()
+        players = list(filter(filter_price_and_position, players))
 
-    print(my_team)
+    for player in players:
+        print(f"{player.first_name} {player.second_name} - {player.now_cost / 10}\n")
 
-print(os.environ["FPL_EMAIL"])
-print(os.environ["FPL_PASSWORD"])
+
+def filter_price_and_position(player):
+    if player.element_type == position and player.now_cost <= max_price:
+        return True
+
+    return False
+
+
+def position_number(pos):
+    match pos:
+        case "gkp":
+            return 1
+        case "def":
+            return 2
+        case "mid":
+            return 3
+        case "fwd":
+            return 4
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--price', help='Max price of the player')
+parser.add_argument('--pos', help='Which position the player should be')
+
+args = parser.parse_args()
+max_price = int(float(args.price) * 10)
+position = position_number(args.pos.lower())
+
+print(max_price)
+print(position)
+
 asyncio.run(main())
