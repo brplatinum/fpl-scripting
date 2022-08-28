d39fd523a77c3a2f56f6e61181b9caebae76a649
Reece White <reecethomaswhite@gmail.com>
2022-08-28 19:02:29 +1000

    Intial FPL python package script

--- which_player.py
+++ which_player.py
@@ -1,1 +1,17 @@
+import asyncio
+import aiohttp
 from fpl import FPL
+
+# Need to set system environment variables FPL_EMAIL and FPL
+async def main():
+    async with aiohttp.ClientSession() as session:
+        fpl = FPL(session)
+        await fpl.login()
+        user = await fpl.get_user()
+        my_team = await user.get_team()
+
+    print(my_team)
+
+print(os.environ["FPL_EMAIL"])
+print(os.environ["FPL_PASSWORD"])
+asyncio.run(main())
